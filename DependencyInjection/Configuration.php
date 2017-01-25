87bc86d9939966ca22e68ab5c5fe6b0b8850d830
Olivier <sabinus52@gmail.com>
2017-01-25 22:40:09 +0100

    Génération du bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Olix\ImportFluxBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('olix_import_flux');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('partner_class')->cannotBeEmpty()->defaultValue('Olix\ImportFluxBundle\Entity\Partner')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ OlixImportFluxBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Olix\ImportFluxBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OlixImportFluxBundle extends Bundle
+{
+}
