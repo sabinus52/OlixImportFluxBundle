a02d699fbb36d44dbd5678b76772307294f225ff
Olivier <sabinus52@gmail.com>
2019-08-17 18:00:23 +0200

    Filtre pour un ensemble de rubrique

--- Repository/PartnerRepository.php
+++ Repository/PartnerRepository.php
@@ -20,15 +20,15 @@ class PartnerRepository extends EntityRepository
     /**
      * Retourne les partenaires actifs d'une rubrique donnée
      *
-     * @param integer $rubric : Rubrique dont fait partie les partenaires
-     * @param array   $list   : Liste des partenaires à retourner
+     * @param array $rubric : Rubrique dont fait partie les partenaires
+     * @param array $list   : Liste des partenaires à retourner
      * @return array
      */
-    public function findActiveByRubric($rubric, array $list = array())
+    public function findActiveByRubric(array $rubric, array $list = array())
     {
         $query = $this->createQueryBuilder('p')
             ->where('p.active = :active')
-            ->andWhere('p.rubric = :rubric')
+            ->andWhere('p.rubric IN (:rubric)')
             ->setParameter(':active', true)
             ->setParameter(':rubric', $rubric);
         if ($list) $query = $query->andWhere($query->expr()->in('p.code', $list));
@@ -41,17 +41,17 @@ class PartnerRepository extends EntityRepository
     /**
      * Active le mode "en attente d'execution" pour les partenaires actifs d'une rubrique donnée
      *
-     * @param integer $rubric : Rubrique dont fait partie les partenaires
-     * @param array   $list   : Liste des partenaires à retourner
+     * @param array $rubric : Rubrique dont fait partie les partenaires
+     * @param array $list   : Liste des partenaires à retourner
      * @return array
      */
-    public function updateStatusWaitForActiveByRubric($rubric, array $list = array())
+    public function updateStatusWaitForActiveByRubric(array $rubric, array $list = array())
     {
         $query = $this->createQueryBuilder('p')
             ->update()
             ->set('p.doStatus', Partner::STATUS_WAIT)
             ->where('p.active = :active')
-            ->andWhere('p.rubric = :rubric')
+            ->andWhere('p.rubric IN (:rubric)')
             ->setParameter(':active', true)
             ->setParameter(':rubric', $rubric);
         if ($list) $query = $query->andWhere($query->expr()->in('p.code', $list));
